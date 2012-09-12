2c2b3757ada68108dca1030e9770fa0122ad00da
Justin Lilly <justin@justinlilly.com>
2012-09-12 12:26:44 -0700

    Adding support for gravatar fallback as well as hashed email addresses.

--- robotars/templatetags/robotars_tags.py
+++ robotars/templatetags/robotars_tags.py
@@ -1,14 +1,21 @@
 # http://robohash.org/
 
 from django import template
-
+from md5 import md5
 
 register = template.Library()
 
-
 @register.inclusion_tag("robotars/robotar.html")
-def robotar(user, size=None):
-    url = "http://robohash.org/%s" % user
+def robotar(user, size=None, gravatar_fallback=False, hashed=False):
+    _user = user
+    if hashed:
+        _user = md5(user).hexdigest()
+    url = "http://robohash.org/%s?" % _user
     if size is not None:
-        url += "?size=%s" % size
+        url += 'size=%s' % size
+    if gravatar_fallback:
+        sep = "?"
+        if "?size=" in url:
+            sep = "&"
+        url += "%sgravatar=%s" % (sep, "hashed" if hashed else "yes")
     return {"robotar_url": url, "robotar_user": user}
